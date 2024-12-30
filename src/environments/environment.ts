69438df6aaae111566a3e732c97c2f64924074cf
codetobuild <goodnakha@gmail.com>
2024-12-30 21:27:59 +0530

    fix: typo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'https://wrap-2024-nodejs.onrender.com/api',
-  // imageBaseUrl: 'https://wrap-2024-nodejs.onrender.com',
-  apiBaseUrl: 'http://localhost:8080/api',
-  imageBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: 'https://wrap-2024-nodejs.onrender.com/api',
+  imageBaseUrl: 'https://wrap-2024-nodejs.onrender.com',
+  // apiBaseUrl: 'http://localhost:8080/api',
+  // imageBaseUrl: 'http://localhost:8080',
 };
