ef6c85202d709a9a2c3ecda144c725427278a364
codetobuild <goodnakha@gmail.com>
2024-12-30 21:01:04 +0530

    fix: css responsive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://wrap-2024-nodejs.onrender.com/api',
   imageBaseUrl: 'https://wrap-2024-nodejs.onrender.com',
+  // apiBaseUrl: 'http://localhost:8080/api',
+  // imageBaseUrl: 'http://localhost:8080',
 };
